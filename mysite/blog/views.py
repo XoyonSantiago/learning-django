84e44973125f69f7de3f8ba75cdc8a756b5a351e
Santiago <xoyonsantiago@gmail.com>
2019-03-27 22:22:02 -0600

    Templates modificados para mostrar post desde base de datos.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+# agramos el metodo Post del archivo models
+# significa que podemos utilizar . y el nombre del archivo (sin .py)
+from .models import Post
+
+# importamos esta libreria de fechas de django
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # publicamos los posto por fechas
+    return render(request, 'blog/post_list.html', {'posts': posts}) # 'nombre de la varialve posts'
